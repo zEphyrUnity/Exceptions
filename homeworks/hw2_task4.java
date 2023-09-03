08fafee5723b589374f73671d810df4bec0f9c96
zEphyr <stelzz@ya.ru>
2023-09-03 21:17:19 +0300

    homework 2

---
+++ homeworks/hw2_task4.java
@@ -0,0 +1,23 @@
+//Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+//Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+package homeworks;
+
+import java.util.Scanner;
+
+public class hw2_task4 {
+    public static void main(String[] args) throws Exception {
+        printUserText();
+    }
+
+    public static void printUserText() throws Exception {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        if(input.isEmpty()){
+            throw new Exception("Пустые строки вводить нельзя!");
+        }else{
+            System.out.println("Вы ввели: " + input);
+        }
+    }
+}
